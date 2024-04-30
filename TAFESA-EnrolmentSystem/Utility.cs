e0877d82c9b24c4ff01e85db4e28793ada24d48c
boral <boralghost@student.tafesa.edu.au>
2024-04-30 19:10:03 +0930

    Created Utility class to implement search methods using Linear and Binary searches.

---
+++ TAFESA-EnrolmentSystem/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAFESA_EnrolmentSystem
+{
+    public class Utility
+    {
+        public static int LinearSeachArray<T>(T[] array, T target) where T : IComparable<T>
+        { 
+            
+        }
+
+        public static int BinarySearchArray<T>(T[] array, T target) where T : IComparable<T>
+        { 
+        
+        }
+    }
+}
